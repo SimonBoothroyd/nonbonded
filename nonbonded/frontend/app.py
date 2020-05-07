e33023c5f07e0a7787e01f03204c019ff4ffc7ea
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-05-06 18:49:16 -0600

    Remove now defunct frontend modules.

--- nonbonded/frontend/app.py
+++
@@ -1,7 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-external_stylesheets = [dbc.themes.BOOTSTRAP]
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.config.suppress_callback_exceptions = True
