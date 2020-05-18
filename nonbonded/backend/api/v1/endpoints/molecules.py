bd095d3c7270e9f46860e259b51e989b1fd45d23
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-05-18 09:29:45 -0600

    Reactivate molecule image endpoint.

--- nonbonded/backend/api/v1/endpoints/molecules.py
+++ nonbonded/backend/api/v1/endpoints/molecules.py
@@ -1,19 +1,19 @@
 from fastapi import APIRouter
 
-# from starlette.responses import Response
+from starlette.responses import Response
 
-# from nonbonded.library.models.datasets import Substance
-# from nonbonded.library.utilities.molecules import smiles_to_image
+from nonbonded.library.utilities.molecules import smiles_to_image
+from nonbonded.library.utilities.molecules import url_string_to_smiles
 
 router = APIRouter()
 
 
-# @router.get("/image/{smiles}")
-# async def get_molecule_image(smiles):
-#
-#     substance = Substance.from_url_string(smiles)
-#
-#     svg_content = smiles_to_image(substance.smiles)
-#     svg_response = Response(svg_content, media_type="image/svg+xml")
-#
-#     return svg_response
+@router.get("/{smiles}/image")
+async def get_molecule_image(smiles: str):
+
+    smiles = url_string_to_smiles(smiles)
+
+    svg_content = smiles_to_image(smiles)
+    svg_response = Response(svg_content, media_type="image/svg+xml")
+
+    return svg_response
--- nonbonded/library/utilities/molecules.py
+++ nonbonded/library/utilities/molecules.py
@@ -4,30 +4,32 @@ from cmiles.utils import load_molecule
 from rdkit.Chem import rdDepictor
 from rdkit.Chem.Draw import rdMolDraw2D
 
+import urllib.parse
+
 
 @functools.lru_cache(500)
-def smiles_to_image(smiles_tuple):
+def smiles_to_image(smiles: str):
 
-    rdkit_molecules = [
-        load_molecule(smiles, toolkit="rdkit") for smiles in smiles_tuple
-    ]
+    rdkit_molecule = load_molecule(smiles, toolkit="rdkit")
 
-    for rdkit_molecule in rdkit_molecules:
+    if not rdkit_molecule.GetNumConformers():
+        rdDepictor.Compute2DCoords(rdkit_molecule)
 
-        if not rdkit_molecule.GetNumConformers():
-            rdDepictor.Compute2DCoords(rdkit_molecule)
-
-    n_molecules = len(smiles_tuple)
-
-    drawer = rdMolDraw2D.MolDraw2DSVG(n_molecules * 200, 200, 150, 200)
+    drawer = rdMolDraw2D.MolDraw2DSVG(200, 200, 150, 200)
     drawer.drawOptions().padding = 0.05
 
-    for index, rdkit_molecule in enumerate(rdkit_molecules):
-
-        drawer.SetOffset(index * 200 + 25, 0)
-        drawer.DrawMolecule(rdkit_molecule)
+    drawer.SetOffset(25, 0)
+    drawer.DrawMolecule(rdkit_molecule)
 
     drawer.FinishDrawing()
 
     svg = drawer.GetDrawingText()
     return svg
+
+
+def smiles_to_url_string(smiles):
+    return urllib.parse.quote(smiles)
+
+
+def url_string_to_smiles(url_string):
+    return urllib.parse.unquote(url_string)
