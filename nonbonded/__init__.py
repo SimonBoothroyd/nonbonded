0bc2822581ee0d26fbd3ffd7db7129f371a71837
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-05-25 09:24:42 -0600

    Handle exception only seen on heroku.

--- nonbonded/__init__.py
+++ nonbonded/__init__.py
@@ -6,7 +6,10 @@ Automated optimization and assesment of nonbonded force field parameters
 # Handle versioneer
 from ._version import get_versions
 
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
+try:
+    versions = get_versions()
+    __version__ = versions["version"]
+    __git_revision__ = versions["full-revisionid"]
+    del get_versions, versions
+except ValueError:
+    pass
