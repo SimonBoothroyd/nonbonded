53f3e8f0f365183860dbbad0868135c396a2481d
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-05-29 15:35:14 -0600

    Initial attempt to allow restarting jobs.

--- nonbonded/cli/benchmark/run.py
+++ nonbonded/cli/benchmark/run.py
@@ -1,3 +1,7 @@
+import logging
+import os
+from typing import Optional
+
 import click
 
 from nonbonded.cli.options.evaluator import EvaluatorServerConfig
@@ -6,6 +10,8 @@ from nonbonded.library.utilities.logging import (
     setup_timestamp_logging,
     string_to_log_level,
 )
+
+logger = logging.getLogger(__name__)
 
 
 @click.command(
@@ -28,15 +34,23 @@ from nonbonded.library.utilities.logging import (
     show_default=True,
 )
 @click.option(
+    "--restart",
+    default=True,
+    type=click.BOOL,
+    help="Whether to restart any failed estimations if found. If false, any "
+    "existing results will be overwritten.",
+    show_default=True,
+)
+@click.option(
     "--log-level",
     default="info",
     type=click.Choice(get_log_levels()),
     help="The verbosity of the server logger.",
     show_default=True,
 )
-def run(server_config, polling_interval, log_level):
+def run(server_config, polling_interval: int, restart: bool, log_level):
 
-    from openff.evaluator.client import ConnectionOptions, EvaluatorClient
+    from openff.evaluator.client import ConnectionOptions, EvaluatorClient, RequestResult
     from openff.evaluator.datasets import PhysicalPropertyDataSet
 
     from openforcefield.typing.engines.smirnoff import ForceField
@@ -47,11 +61,46 @@ def run(server_config, polling_interval, log_level):
     if logging_level is not None:
         setup_timestamp_logging(logging_level)
 
+    # Check for existing results files to restart from.
+    existing_results: Optional[RequestResult] = None
+
+    if os.path.isfile("results.json"):
+
+        message = "An existing results file was found."
+
+        if not restart:
+            message = f"{message} These results will be overwritten."
+        else:
+
+            existing_results: RequestResult = RequestResult.from_json("results.json")
+
+            if len(existing_results.unsuccessful_properties) == 0:
+
+                message = (
+                    f"{message} All properties were successfully estimated and so "
+                    f"this command will now exit."
+                )
+
+                logger.info(message)
+                return
+
+            message = (
+                f"{message} {len(existing_results.estimated_properties)} data points "
+                f"were successfully estimated, while "
+                f"{len(existing_results.unsuccessful_properties)} could not be. "
+                f"Attempting to re-estimate these unsuccessful data points."
+            )
+
+        logger.info(message)
+
     # Load in the force field.
     force_field = ForceField("force-field.offxml")
 
     # Load in the data set.
-    data_set = PhysicalPropertyDataSet.from_json("test-set.json")
+    if existing_results is None:
+        data_set = PhysicalPropertyDataSet.from_json("test-set.json")
+    else:
+        data_set = existing_results.unsuccessful_properties
 
     server_config = EvaluatorServerConfig.parse_file(server_config)
 
@@ -79,5 +128,19 @@ def run(server_config, polling_interval, log_level):
             if error is not None:
                 raise Exception(str(error))
 
+            # Save a copy of the results in case adding the already estimated
+            # properties failed for some reason.
+            results.json("results.tmp.json")
+
+            if existing_results is not None:
+
+                results.estimated_properties.add_properties(
+                    *existing_results.estimated_properties.properties, validate=False
+                )
+
             # Save the results to disk.
             results.json("results.json")
+
+            if os.path.isfile("results.tmp.json"):
+                # Remove the backup results.
+                os.unlink("results.tmp.json")
--- nonbonded/cli/optimization/run.py
+++ nonbonded/cli/optimization/run.py
@@ -1,13 +1,20 @@
+import logging
+import os
+import shutil
+from glob import glob
 from subprocess import check_call
 
 import click
 
 from nonbonded.cli.options.evaluator import EvaluatorServerConfig
+from nonbonded.library.models.projects import Optimization
 from nonbonded.library.utilities.logging import (
     get_log_levels,
     setup_timestamp_logging,
     string_to_log_level,
 )
+
+logger = logging.getLogger(__name__)
 
 
 @click.command(
@@ -23,13 +30,22 @@ from nonbonded.library.utilities.logging import (
     show_default=True,
 )
 @click.option(
+    "--restart",
+    default=True,
+    type=click.BOOL,
+    help="Whether to restart the optimization from where it left off if one was "
+    "already in progress and failed. If false, any existing results will be "
+    "overwritten.",
+    show_default=True,
+)
+@click.option(
     "--log-level",
     default="info",
     type=click.Choice(get_log_levels()),
     help="The verbosity of the server logger.",
     show_default=True,
 )
-def run(server_config, log_level):
+def run(server_config, restart: bool, log_level):
 
     # Set up logging if requested.
     logging_level = string_to_log_level(log_level)
@@ -37,9 +53,96 @@ def run(server_config, log_level):
     if logging_level is not None:
         setup_timestamp_logging(logging_level)
 
+    # Load in the optimization being performed.
+    optimization = Optimization.parse_file("optimization.json")
+
+    # Remove any residual files if not restarting.
+    should_restart = False
+
+    if not restart:
+
+        restart_files = [
+            "optimize.tmp",
+            "optimize.bak",
+            "optimize.sav",
+            "result",
+            "worker-logs",
+            "working-data",
+        ]
+
+        for restart_file in restart_files:
+
+            if os.path.isdir("optimize.tmp") or os.path.isfile(restart_file):
+
+                logger.info(f"Removing {restart_file} as restarting is disabled.")
+
+                if os.path.isdir(restart_file):
+                    shutil.rmtree(restart_file)
+                else:
+                    os.unlink(restart_file)
+
+    else:
+
+        target_name = optimization.force_balance_input.target_name
+
+        previous_iteration_directories = glob(
+            os.path.join("optimize.tmp", target_name, "iter_*")
+        )
+
+        if (
+            not os.path.isfile("optimize.sav")
+            and len(previous_iteration_directories) > 0
+        ):
+
+            raise Exception(
+                "Previous optimization ouput directories were found but optimize.sav "
+                "was not."
+            )
+
+        if os.path.isfile("optimize.sav") and len(previous_iteration_directories) == 0:
+
+            raise Exception(
+                "An optimize.sav file was found but no optimization output directories "
+                "could be found."
+            )
+
+        n_iterations = len(previous_iteration_directories)
+
+        if n_iterations > 0:
+
+            should_restart = True
+
+            for iteration in range(n_iterations):
+
+                expected_directory = os.path.join(
+                    "optimize.tmp", target_name, "iter_" + str(iteration).zfill(4)
+                )
+
+                if not os.path.isdir(expected_directory):
+                    raise Exception(f"The {expected_directory} directory is missing.")
+
+            final_directory = os.path.join(
+                "optimize.tmp", target_name, "iter_" + str(n_iterations - 1).zfill(4)
+            )
+
+            if not os.path.isfile(os.path.join(final_directory, "results.json")):
+                # Remove the last directory if it did not contain a results file.
+                shutil.rmtree(final_directory)
+                n_iterations -= 1
+
+            logger.info(
+                f"{n_iterations} iterations had previously been completed. "
+                f"The optimization will be restarted from iteration {n_iterations}"
+            )
+
     server_config = EvaluatorServerConfig.parse_file(server_config)
 
     calculation_backend = server_config.to_backend()
+
+    force_balance_arguments = ["ForceBalance.py", "optimize.in"]
+
+    if should_restart:
+        force_balance_arguments.insert(1, "--continue")
 
     with calculation_backend:
 
@@ -48,4 +151,4 @@ def run(server_config, log_level):
         with evaluator_server:
 
             with open("force_balance.log", "w") as file:
-                check_call(["ForceBalance.py", "optimize.in"], stdout=file)
+                check_call(force_balance_arguments, stderr=file, stdout=file)
